f9adec252ca070b85822e14fae02e901d3d2b914
sabincoding01 <sabintimalsina74@gmail.com>
2025-07-03 21:34:19 +0545

    This is my first SasProject

---
+++ server.ts
@@ -0,0 +1,15 @@
+import app  from "./src/app";
+import { envConfig } from "./src/config/config";
+
+function startServer(){
+    const port = envConfig.portNumber
+    app.listen(port,function(){
+
+    console.log(`server has started at port ${port}`);
+
+    
+    })
+
+}
+
+startServer()
